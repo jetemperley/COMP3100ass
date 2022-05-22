5abf70176d7d1e0f38f9b6daa7e575f1e9c99921
jacob <jetemperley@gmail.com>
2022-05-22 22:20:54 +1000

    created First Capable algorithm

---
+++ FC.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+
+public class FC implements Scheduler {
+	
+	
+	
+	public void schedule(String[] job, Client client){
+		
+		client.send("GETS Capable " + job[4] +" " +job[5] +" " +job[6]);
+		client.receive();
+
+		client.send("OK");
+		List<String> servers = client.receive();
+		
+		client.send("OK");
+		client.receive();
+		
+		String[] srv = servers.get(0).split(" ");
+		int jobID = Integer.parseInt(job[2]);
+		
+		client.send("SCHD " +
+			jobID + " " +
+			srv[0] + " " +
+			Integer.parseInt(srv[1]));
+		client.receive();
+		
+		
+	
+	}
+	
+}
