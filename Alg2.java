dcd79e8bb6f9d8ff3d83ec4a579050f9ad8e75a4
jacob <jetemperley@gmail.com>
2022-05-24 14:14:56 +1000

    getting started with the 2nd try at a new algorithm

---
+++ Alg2.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class Alg1 implements Scheduler {
+	
+	
+	
+	public void schedule(String[] job, Client client){
+		
+		List<String> servers = client.getServers("Capable ", job[4], job[5], job[6]);
+		
+		
+		
+	}
+	
+}
